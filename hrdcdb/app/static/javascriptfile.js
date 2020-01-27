0460fa88db4cc684630947207c8db3f3175579b3
Joseph-Akers <jjg.akers@gmail.com>
2020-01-26 17:05:42 -0700

    added a javascript file to alert when delete btn is clicked

---
+++ hrdcdb/app/static/javascriptfile.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+//    if (document.getElementsByClassName("delBtn")){
+//        // assign button to a variable
+//        var delBtn = document.getElementById("delBtn")
+//        
+//        //add an alert when click
+//        delBtn.addEventListener("click", function(){
+//            alert('Are you sure you want to delete this record?')
+//        })
+//    }
+    $(".delBtn").on( "click", function() {
+        alert("something")
+    })
+
+}
